6d92b13251c0cb310db923d6be1af8c90261ad54
GentleXD <alexanderpadberg33@gmail.com>
2019-12-08 22:46:36 +0100

    [0.0.1] Init - Creating the basic LibGDX Project. Executed the basic gradle commands for initialisation and compiling. Changed Java version to 1.8.

---
+++ desktop/src/de/undefinedhuman/sandboxgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.undefinedhuman.sandboxgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.undefinedhuman.sandboxgame.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
