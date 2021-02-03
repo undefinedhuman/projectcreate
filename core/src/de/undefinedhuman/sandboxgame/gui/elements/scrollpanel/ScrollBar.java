a1297cd79045585d29125a4a1bcac718753dae97
undefinedhuman <alexander.padberg@code.berlin>
2021-02-04 00:12:31 +0100

    add scroll panel arrow textures

---
+++ core/src/de/undefinedhuman/sandboxgame/gui/elements/scrollpanel/ScrollBar.java
@@ -0,0 +1,12 @@
+package de.undefinedhuman.sandboxgame.gui.elements.scrollpanel;
+
+import de.undefinedhuman.sandboxgame.gui.Gui;
+import de.undefinedhuman.sandboxgame.gui.texture.GuiTemplate;
+
+public class ScrollBar extends Gui {
+
+    public ScrollBar(GuiTemplate template, int width) {
+        super(template);
+    }
+
+}
