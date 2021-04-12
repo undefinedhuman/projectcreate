99ba02f98d0d7df67b30119bcb973115556d1688
alex <alexander.padberg@code.berlin>
2021-04-12 18:28:37 +0200

    ci: test jenkins gitlab integration

--- core/src/de/undefinedhuman/projectcreate/core/Main.java
+++ core/src/de/undefinedhuman/projectcreate/core/Main.java
@@ -41,7 +41,7 @@ public class Main extends Game {
     private Timer timer;
 
     public Main() {
-        // TEST
+        // TEST COMMIT
 
         instance = this;
         managerList = new ManagerList();
