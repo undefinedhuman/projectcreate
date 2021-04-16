62be778f05883adbe36d9255c7117acba2f35059
alex <alexander.padberg@code.berlin>
2021-04-16 02:32:39 +0200

    ci: check if test reports exists

--- core/test/CoreTest.java
+++ core/test/CoreTest.java
@@ -1,3 +1,4 @@
+import de.undefinedhuman.projectcreate.core.utils.Tools;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -7,7 +8,9 @@ class CoreTest {
 
     @Test
     void testCore() {
-        Assertions.assertThat(true).isTrue();
+        Assertions.assertThat(Tools.mix(0, 1f, 0)).isEqualTo(0f);
+        Assertions.assertThat(Tools.mix(0, 1f, 0.5f)).isEqualTo(0.5f);
+        Assertions.assertThat(Tools.mix(0, 1f, 1f)).isEqualTo(1f);
     }
 
 }
