52f68224629eac1fbe7f00cffc3403bcc62c36d7
alex <alexander.padberg@code.berlin>
2021-04-18 16:07:34 +0200

    chore: test push

--- core/src/de/undefinedhuman/projectcreate/core/Main.java
+++ core/src/de/undefinedhuman/projectcreate/core/Main.java
@@ -41,7 +41,7 @@ public class Main extends Game {
     private Timer timer;
 
     public Main() {
-        // TEST PUSH 4
+        // TEST PUSH 5
 
         instance = this;
         managerList = new ManagerList();
