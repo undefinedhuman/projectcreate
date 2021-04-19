050becc9e693fef897892c0c5cd0721ada0e97e1
alex <alexander.padberg@code.berlin>
2021-04-20 00:41:43 +0200

    filler commit

--- core/src/de/undefinedhuman/projectcreate/core/Main.java
+++ core/src/de/undefinedhuman/projectcreate/core/Main.java
@@ -41,7 +41,7 @@ public class Main extends Game {
     private Timer timer;
 
     public Main() {
-        // TEST DEV COMMIT 1
+        // TEST DEV COMMIT 2
 
         instance = this;
         managerList = new ManagerList();
