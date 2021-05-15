706e0202eecb0b592c1530a1d9221922f6e19d7a
alex <alexander.padberg@code.berlin>
2021-05-16 00:57:51 +0200

    fix: include numbersetting

---
+++ engine/src/de/undefinedhuman/projectcreate/engine/settings/types/primitive/NumberSetting.java
@@ -0,0 +1,26 @@
+package de.undefinedhuman.projectcreate.engine.settings.types.primitive;
+
+import de.undefinedhuman.projectcreate.engine.log.Log;
+import de.undefinedhuman.projectcreate.engine.settings.Setting;
+import de.undefinedhuman.projectcreate.engine.settings.interfaces.Parser;
+
+import java.lang.reflect.ParameterizedType;
+
+public abstract class NumberSetting<T extends Number & Comparable<T>> extends Setting<T> {
+
+    public NumberSetting(String key, T defaultValue, Parser<T> parse, T min, T max) {
+        super(key, defaultValue, value -> {
+            T parsedValue = parse.get(String.valueOf(value));
+            if(parsedValue == null) {
+                Log.error("Error while parsing as " + ((Class<T>) ((ParameterizedType) parse.getClass().getGenericSuperclass()).getActualTypeArguments()[0]).getTypeName() + ": " + value);
+                return defaultValue;
+            }
+            if(parsedValue.compareTo(min) < 0 || parsedValue.compareTo(max) > 0) {
+                Log.error("Out of range: Range [" + min + ", " + max + "], Value: " + parsedValue);
+                return defaultValue;
+            }
+            return parsedValue;
+        });
+    }
+
+}
